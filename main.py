12e3d6fe1856d0c5279823aef749ade1dda4792e
user <qw@qw.local>
2021-03-20 05:51:52 +0300

    lesson_4

--- main.py
+++ main.py
@@ -1,103 +1,89 @@
 import requests
-from bs4 import BeautifulSoup as bs
-from pymongo import MongoClient
+from lxml import html
+from datetime import datetime
+from pprint import pprint
 
-CLIENT = MongoClient('localhost', 27017)
-DB = CLIENT['db_hh']
-DB_CURSOR = DB.users
 
-MAIN_LINK = 'https://hh.ru/'
+LINKS = {'mail': 'https://news.mail.ru/',
+         'lenta': 'https://lenta.ru/',
+         'yandex': 'https://yandex.ru/'
+         }
+
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'}
 
 
-class Parser():
-    def __init__(self, link, headers, db_cursor):
-        self.link = link
+class Parser:
+    def __init__(self, links_dict, headers):
+        self.links_dict = links_dict
         self.headers = headers
-        self.db_cursor = db_cursor
+        self.result_news = []
 
-    def get_vacancies_by_salary(self, value):
-        result = self.db_cursor.find({'min_salary': {'$gte': value}})
-        for item in result:
-            print(f'{item}\n')
+    def router(self):
+        for name, value in self.links_dict.items():
+            getattr(self, f'parse_{name}', None)(value)
+        pprint(self.result_news)
 
-    def parse_data(self, position):
-        page = 0
-        vacancies = []
-        params = {
-            'area': '1624',
-            'st': 'searchVacancy',
-            'fromSearchLine': 'true',
-            'text': position,
-            'page': page
-        }
-        while True:
-            params['page'] = page
-            response = requests.get(f'{self.link}/search/vacancy', params=params, headers=self.headers)
-            if response.ok:
-                soup = bs(response.content, 'html.parser')
-                vacancies_soup = soup.findAll('div', attrs={'class': 'vacancy-serp-item'})
-                for item in vacancies_soup:
-                    vacancy_dict = {}
-                    vacancy_a = item.find('a', attrs={'class': 'bloko-link'})
-                    link = vacancy_a['href']
-                    name = vacancy_a.getText()
-                    salary_obj = item.find('div', attrs={
-                        'class': 'vacancy-serp-item__sidebar'}).find('span')
-                    if salary_obj:
-                        salary = salary_obj.getText()
-                        currency = salary[-4:]
-                        if 'от' in salary:
-                            min_salary = salary[3:-4].strip().replace('\xa0', '')
-                            max_salary = 0
-                        elif 'до' in salary:
-                            min_salary = 0
-                            max_salary = salary[3:-4].strip().replace('\xa0', '')
-                        else:
-                            min_salary, max_salary = [s.replace('\xa0', '') for s in salary[:-4].strip().split('-')]
-                    else:
-                        min_salary = 0
-                        max_salary = 0
-                        currency = 'Не указано'
-                    vacancy_dict['vacancy_name'] = name
-                    vacancy_dict['vacancy_link'] = link
-                    vacancy_dict['min_salary'] = int(min_salary)
-                    vacancy_dict['max_salary'] = int(max_salary)
-                    vacancy_dict['currency'] = currency
-                    vacancy_dict['site_link'] = self.link
-                    vacancies.append(vacancy_dict)
-                if soup.find('a', attrs={'class': 'HH-Pager-Controls-Next'}):
-                    page += 1
-                else:
-                    for item in vacancies:
-                        if not self.data_exists(item):
-                            self.db_cursor.insert_one(item)
-                    break
+    def parse_mail(self, url):
+        response = requests.get(url, headers=HEADERS)
+        dom = html.fromstring(response.text)
+        items = dom.xpath(
+            '//a[contains(@class,"photo photo_full photo_scale")] | //a[contains(@class,"photo photo_small photo_scale photo_full js-topnews__item")] ')
+        i = 0
+        for item in items:
+            news = {}
+            name = item.xpath('.//span[@class="photo__title photo__title_new photo__title_new_hidden js-topnews__notification"]/text()')[0].replace('\xa0', ' ')
+            url_new = item.xpath('//a[contains(@class, "js-topnews__item")]/@href')[i]
+            news['name'] = name
+            news['url'] = url_new
+            source, date = self.get_news_data(url_new)
+            news['source'] = source
+            news['date'] = date
+            self.result_news.append(news)
+            i += 1
 
-    def data_exists(self, item):
-        if self.db_cursor.find_one(item):
-            return True
-        return False
+    def get_news_data(self, link):
+        response = requests.get(link, headers=HEADERS)
+        dom = html.fromstring(response.text)
+        items = dom.xpath('//div[contains(@class,"article js-article")]')
+        for item in items:
+            source = item.xpath('.//span[@ class ="link__text"]/text()')[0]
+            date = item.xpath('//span[@ class ="note__text breadcrumbs__text js-ago"]/@ datetime')[0]
+            return source, date
+
+    def parse_lenta(self, url):
+        response = requests.get(url, headers=HEADERS)
+        dom = html.fromstring(response.text)
+        items = dom.xpath('//div[@class="b-yellow-box__wrap"]/div[@class="item"]')
+        source = "lenta.ru"
+        for item in items:
+            news = {}
+            name = item.xpath('.//a/text()')[0].replace('\xa0',' ')
+            url_new = item.xpath('.//a/@href')[0]
+            day_list = url_new.replace('/news/', '').split('/')
+            day = day_list.pop(0) + '.' + day_list.pop(0) + '.' + day_list.pop(0)
+            news['name'] = name
+            news['link'] = url + url_new
+            news['source'] = source
+            news['date'] = day
+            self.result_news.append(news)
+
+    def parse_yandex(self, url):
+        response = requests.get(url, headers=HEADERS)
+        dom = html.fromstring(response.text)
+        items = dom.xpath('//ol/li')
+        date = datetime.today().strftime("%d-%m-%Y")
+        for item in items:
+            news = {}
+            source = item.xpath('.//object/@title')[0]
+            name = item.xpath('.//span[contains(@class ,"news__item-content")]/text()')[0]
+            url_new = item.xpath('.//a/@href')[0]
+            news['source'] = source
+            news['link'] = url_new
+            news['name'] = name
+            news['date'] = date
+            self.result_news.append(news)
 
 
-parser = Parser(MAIN_LINK, HEADERS, DB_CURSOR)
+parser = Parser(LINKS, HEADERS)
 
-while True:
-    reaction = input('Хотите парсить? Нажмите: 1\n'
-                     'Хотите посмотреть вакансии? Нажмите: 2\n'
-                     '(для выхода нажмите "q"): ')
-    if reaction == '1':
-        position = input('Введите поисковую фразу (для выхода нажмите "q"): ')
-        if position == 'q':
-            break
-        else:
-            print('Собираю данные...')
-            parser.parse_data(position)
-    if reaction == '2':
-        salary = int(input('Введите зп для поиска (для выхода нажмите "q"): '))
-        if salary == 'q':
-            break
-        else:
-            parser.get_vacancies_by_salary(salary)
-    if reaction == 'q':
-        break
+parser.router()
